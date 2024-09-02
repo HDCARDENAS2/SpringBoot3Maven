256e6e3b85375952f87c9d002631d13bd71b35ab
hdcardenas <dropimax@gmail.com>
2024-09-02 15:46:59 -0500

    se suben cambios por version de H2 y deprecated de sequence

--- src/main/java/com/learn/springb3/entity/UserEntity.java
+++ src/main/java/com/learn/springb3/entity/UserEntity.java
@@ -3,20 +3,21 @@ package com.learn.springb3.entity;
 import java.io.Serializable;
 import java.util.Date;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
 
 @Entity
 @AllArgsConstructor
@@ -32,8 +33,11 @@ public class UserEntity implements Serializable {
 	private static final long serialVersionUID = -6659355453167373930L;
 	
 	@Id
-	@SequenceGenerator(name="USER_GENERATOR_SEQ", sequenceName = "USER_SEQ")
-	@GeneratedValue(generator = "USER_GENERATOR_SEQ")
+	@GeneratedValue(generator = "USER_SEQ", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(
+        name = "USER_SEQ",
+        allocationSize = 1
+    )
     @Column(name = "id", unique = true, nullable = false)
 	private Integer id;
     @Column(name = "name", nullable = false)
